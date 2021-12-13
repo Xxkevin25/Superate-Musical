8b16c6b05b1387254c4b7fb87e91f709adb5d1be
Xxkevin25 <kenniego88@gmail.com>
2021-12-13 17:19:01 -0500

    New

---
+++ docs/main.js
@@ -0,0 +1,18 @@
+/*Swiper*/
+var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+/*AOS*/
+AOS.init();
+
